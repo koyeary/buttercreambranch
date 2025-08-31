748c7099dcbc3ba823b0463ff237a2f3482be53e
Kat Yeary <katyeary@gmail.com>
2025-08-31 11:59:56 -0400

    include changes to client directory

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,10 +16,14 @@ export default function RootLayout({
       lang="en"
       className="bg-white dark:bg-gray-950 scheme-light dark:scheme-dark"
     >
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className="layout">
+        <header>
+          <nav>{/* navbar */}</nav>
+        </header>
         {children}
+        <footer>
+          <p>&copy; 2025 Mias Bakery</p>
+        </footer>
       </body>
     </html>
   );
