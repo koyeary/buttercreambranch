7f4931fb84534d1576ffd06c725658e1720358e2
Kat Yeary <katyeary@gmail.com>
2025-09-18 11:08:28 -0400

    ssl

--- db/index.js
+++ db/index.js
@@ -2,12 +2,7 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  ssl:
-    process.env.DB_SSL === "true"
-      ? {
-          rejectUnauthorized: process.env.DB_SSL_REJECT_UNAUTHORIZED === "true",
-        }
-      : false,
+  ssl: process.env.DB_SSL,
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
