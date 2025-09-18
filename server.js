847baa23561704d0e51b75c019d470be5d1eaa86
Kat Yeary <katyeary@gmail.com>
2025-09-18 12:07:13 -0400

    new build

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const routes = require("./routes");
-const pool = require("./db");
+//const pool = require("./db");
 const app = express();
 const cors = require("cors");
 
