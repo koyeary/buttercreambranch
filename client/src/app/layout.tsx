e168c705ec04841e6fbca775069f646df13f3005
Kat Yeary <katyeary@gmail.com>
2025-09-17 21:47:16 -0400

    ...

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import Navbar from "../components/Navbar";
+import Navbar from "@/components/Navbar";
 import Providers from "./providers";
 import "./globals.css";
 
