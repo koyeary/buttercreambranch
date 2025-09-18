787b868c9f83044caaef400f636cf40d44ab516a
Kat Yeary <katyeary@gmail.com>
2025-09-18 10:52:41 -0400

    change pool connection string

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Pool } = require("pg");
 require("dotenv").config();
 
-const pool = new Pool({
+/* const pool = new Pool({
   ssl:
     process.env.DB_SSL === "true"
       ? {
@@ -13,6 +13,13 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PWRD,
   port: process.env.DB_PORT || 5432,
+}); */
+
+const pool = new Pool({
+  connectionString: process.env.CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.on("connect", () => {
